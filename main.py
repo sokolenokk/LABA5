32dcf22a17ac6c7b9ebc52fc26e0c79b1382d8ec
sokolenok <skls77@mail.ru>
2025-12-08 00:27:22 +0300

    Add Main Imports

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import subprocess
+import os
+from datetime import datetime
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QWidget, QTabWidget,
+    QVBoxLayout, QHBoxLayout, QGridLayout, QLabel,
+    QPushButton, QLineEdit, QTextEdit, QProgressBar,
+    QFileDialog, QMessageBox, QSlider, QFrame, QScrollArea
+)
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QFont
+
+
+
